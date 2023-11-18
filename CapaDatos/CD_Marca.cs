9ab9647f4a8452b14c18c66149a9f10adbbae5a6
Sleepwalker <User@DESKTOP-CRJD420>
2023-11-18 13:03:08 -0500

    commit3

---
+++ CapaDatos/CD_Marca.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using CapaEntidad;
+using System.Data.SqlClient;
+using System.Data;
+namespace CapaDatos
+{
+    public class CD_Marca
+    {
+        public List<Marca> Listar()
+        {
+            List<Marca> lista = new List<Marca>();
+            try
+            {
+                using (SqlConnection oconexion = new SqlConnection(Conexion.cn))
+                {
+                    string cnx = "select idMarca,descripcion,activo FROM Marca";
+                    SqlCommand cmd = new SqlCommand(cnx, oconexion);
+                    cmd.CommandType = CommandType.Text;
+                    oconexion.Open();
+                    using (SqlDataReader dr = cmd.ExecuteReader())
+                    {
+                        while (dr.Read())
+                        {
+                            lista.Add(
+                                new Marca()
+                                {
+                                    IdMarca = Convert.ToInt32(dr["IdMarca"]),
+                                    Descripcion = dr["Descripcion"].ToString(),
+                                    Activo = Convert.ToBoolean(dr["Activo"])
+
+                                }
+                                );
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                string error = ex.Message;
+                lista = new List<Marca>();
+            }
+            return lista;
+        }
+
+        public int Registrar(Marca obj, out string Mensaje)
+        {
+            int idautogenerado = 0;
+            Mensaje = string.Empty;
+            try
+            {
+                using (SqlConnection oconexion = new SqlConnection(Conexion.cn))
+                {
+                    SqlCommand cmd = new SqlCommand("usp_RegistrarMarca", oconexion);
+                    cmd.Parameters.AddWithValue("Descripcion", obj.Descripcion);
+                    cmd.Parameters.AddWithValue("Activo", obj.Activo);
+
+                    cmd.Parameters.Add("Resultado", SqlDbType.Int).Direction = ParameterDirection.Output;
+                    cmd.Parameters.Add("Mensaje", SqlDbType.VarChar, 500).Direction = ParameterDirection.Output;
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    oconexion.Open();
+                    cmd.ExecuteNonQuery();
+                    idautogenerado = Convert.ToInt32(cmd.Parameters["Resultado"].Value);
+                    Mensaje = cmd.Parameters["Mensaje"].Value.ToString();
+                }
+            }
+            catch (Exception ex)
+            {
+                idautogenerado = 0;
+                Mensaje = ex.Message;
+            }
+            return idautogenerado;
+        }
+
+        public bool Editar(Marca obj, out string Mensaje)
+        {
+            bool resultado = false;
+            Mensaje = string.Empty;
+            try
+            {
+                using (SqlConnection oconexion = new SqlConnection(Conexion.cn))
+                {
+                    SqlCommand cmd = new SqlCommand("usp_EditarMarca", oconexion);
+                    cmd.Parameters.AddWithValue("idMarca", obj.IdMarca);
+                    cmd.Parameters.AddWithValue("Descripcion", obj.Descripcion);
+                    cmd.Parameters.AddWithValue("Activo", obj.Activo);
+
+                    cmd.Parameters.Add("Resultado", SqlDbType.Bit).Direction = ParameterDirection.Output;
+                    cmd.Parameters.Add("Mensaje", SqlDbType.VarChar, 500).Direction = ParameterDirection.Output;
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    oconexion.Open();
+                    cmd.ExecuteNonQuery();
+                    resultado = Convert.ToBoolean(cmd.Parameters["Resultado"].Value);
+                    Mensaje = cmd.Parameters["Mensaje"].Value.ToString();
+                }
+            }
+            catch (Exception ex)
+            {
+                resultado = false;
+                Mensaje = ex.Message;
+            }
+            return resultado;
+        }
+
+        public bool Eliminar(int id, out string Mensaje)
+        {
+            bool resultado = false;
+            Mensaje = string.Empty;
+            try
+            {
+                using (SqlConnection oconexion = new SqlConnection(Conexion.cn))
+                {
+                    SqlCommand cmd = new SqlCommand("usp_EliminarMarca", oconexion);
+
+                    cmd.Parameters.Add("idMarca", id);
+                    cmd.Parameters.Add("Resultado", SqlDbType.Bit).Direction = ParameterDirection.Output;
+                    cmd.Parameters.Add("Mensaje", SqlDbType.VarChar, 500).Direction = ParameterDirection.Output;
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    oconexion.Open();
+                    cmd.ExecuteNonQuery();
+                    resultado = Convert.ToBoolean(cmd.Parameters["Resultado"].Value);
+                    Mensaje = cmd.Parameters["Mensaje"].Value.ToString();
+                }
+            }
+            catch (Exception ex)
+            {
+                resultado = false;
+                Mensaje = ex.Message;
+            }
+            return resultado;
+        }
+    }
+}
--- CapaNegocio/CN_Producto.cs
+++ CapaNegocio/CN_Producto.cs
@@ -4,10 +4,106 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
+using CapaDatos;
+using CapaEntidad;
+
 namespace CapaNegocio
 {
     public class CN_Producto
     {
+        private CD_Producto objCapaDato = new CD_Producto();
 
+        public List<Producto> Listar() 
+        { 
+           return objCapaDato.listar(); 
+        }
+
+        public int Registrar(Producto obj, out string Mensaje)
+        {
+
+            Mensaje = string.Empty;
+            if (string.IsNullOrEmpty(obj.NomProducto) || string.IsNullOrWhiteSpace(obj.NomProducto))
+            {
+                Mensaje = "El nombre del producto no puede ser vacio";
+            }
+            else if (string.IsNullOrEmpty(obj.DesProducto) || string.IsNullOrWhiteSpace(obj.DesProducto)) 
+            {
+                Mensaje = "La descripcion del producto no puede ser vacio";
+            }
+            else if (obj.oMarca.IdMarca == 0) 
+            {
+                Mensaje = "Debe seleccionar una marca";
+            }
+            else if (obj.oCategoria.IdCategoria == 0) 
+            {
+                Mensaje = "Debe seleccionar una categoria";
+            }
+            else if (obj.Precio == 0) 
+            {
+                Mensaje = "Debe ingresar el precio del producto";
+            }
+            else if (obj.Stock == 0)
+            {
+                Mensaje = "Debe ingresar el stock del producto";
+            }
+            if (string.IsNullOrEmpty(Mensaje))
+            {
+                return objCapaDato.Registrar(obj, out Mensaje);
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        public bool Editar(Producto obj, out string Mensaje)
+        {
+            Mensaje = string.Empty;
+
+            Mensaje = string.Empty;
+            if (string.IsNullOrEmpty(obj.NomProducto) || string.IsNullOrWhiteSpace(obj.NomProducto))
+            {
+                Mensaje = "El nombre del producto no puede ser vacio";
+            }
+            else if (string.IsNullOrEmpty(obj.DesProducto) || string.IsNullOrWhiteSpace(obj.DesProducto))
+            {
+                Mensaje = "La descripcion del producto no puede ser vacio";
+            }
+            else if (obj.oMarca.IdMarca == 0)
+            {
+                Mensaje = "Debe seleccionar una marca";
+            }
+            else if (obj.oCategoria.IdCategoria == 0)
+            {
+                Mensaje = "Debe seleccionar una categoria";
+            }
+            else if (obj.Precio == 0)
+            {
+                Mensaje = "Debe ingresar el precio del producto";
+            }
+            else if (obj.Stock == 0)
+            {
+                Mensaje = "Debe ingresar el stock del producto";
+            }
+            if (string.IsNullOrEmpty(Mensaje))
+            {
+                return objCapaDato.Editar(obj, out Mensaje);
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool GuardarDatosImagen(Producto obj, out string Mensaje)
+        {
+            return objCapaDato.GuardarDatosImagen(obj,out Mensaje);
+        }
+
+        public bool Eliminar(int id, out string Mensaje)
+        {
+           return objCapaDato.Eliminar(id, out Mensaje);
+        }
     }
 }
